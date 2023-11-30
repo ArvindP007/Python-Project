5b5a3d2c63a6901f7df656391ec2a7ff5d68af01
Arvind Patkal <85785725+ArvindP007@users.noreply.github.com>
2023-11-30 18:51:09 +0530

    Update Music_Player.py

--- Music_Player.py
+++ Music_Player.py
@@ -1,48 +1,97 @@
 from tkinter import *
 from tkinter import filedialog
-import os
 import pygame.mixer as mixer
+import os
 
 mixer.init()
-playlist=""
+
+def play_song(song_name: StringVar, songs_list: Listbox, status: StringVar):
+    song_name.set(songs_list.get(ACTIVE))
+
+    mixer.music.load(songs_list.get(ACTIVE))
+    mixer.music.play()
+
+    status.set("Song PLAYING")
+
+
+def stop_song(status: StringVar):
+    mixer.music.stop()
+    status.set("Song STOPPED")
+
+
 def load(listbox):
     os.chdir(filedialog.askdirectory(title='Open a songs directory'))
+
     tracks = os.listdir()
+
     for track in tracks:
         listbox.insert(END, track)
 
-def play_song():
-    #more to be written
-    mixer.music.play()
-    status.set("Song PLAYING")
 
-def stop_song():
-    mixer.music.stop()
-    status.set("Song STOPPED")
-    
-def pause_song():
+def pause_song(status: StringVar):
     mixer.music.pause()
     status.set("Song PAUSED")
-    
-def resume_song():
+
+
+def resume_song(status: StringVar):
     mixer.music.unpause()
     status.set("Song RESUMED")
 
 root = Tk()
-root.title("Music Player")
-root.geometry("1000x200")
+root.geometry('700x220')
+root.title('Music Player')
+root.resizable(0, 0)
 
-trackframe = LabelFrame(root,text="Song Track",font=("times new roman",15,"bold"),bg="Navyblue",fg="white",bd=5,relief=GROOVE)
-# trackframe.place(x=0,y=0,width=600,height=100)
-trackframe.grid(row=1)
-btn_pause = Button(root, text="Pause", command= pause_song, font=("Arial",24), bg='LightBlue')
-btn_pause.grid(row=2, column =1)
-btn_stop = Button(root, text="Stop", command= stop_song, font=("Arial",24), bg='LightBlue')
-btn_stop.grid(row=2, column =2)
-btn_play = Button(root, text="Play", command= play_song, font=("Arial",24), bg='LightBlue')
-btn_play.grid(row=2, column =3)
-btn_resume = Button(root, text="Resume", command= resume_song, font=("Arial",24), bg='LightBlue')
-btn_resume.grid(row=2, column =4)
-btn_load = Button(root, text="Load Directory", command= lambda: load(playlist), font=("Arial",24), bg='LightBlue')
-btn_load.grid(row=3, column=1, columnspan=4)
+song_frame = LabelFrame(root, text='Current Song', bg='Aqua', width=400, height=80)
+song_frame.place(x=0, y=0)
+
+button_frame = LabelFrame(root, text='Control Buttons', bg='Turquoise', width=400, height=120)
+button_frame.place(y=80)
+
+listbox_frame = LabelFrame(root, text='Playlist', bg='RoyalBlue')
+listbox_frame.place(x=400, y=0, height=200, width=300)
+
+current_song = StringVar(root, value='<Not selected>')
+
+song_status = StringVar(root, value='<Not Available>')
+
+playlist = Listbox(listbox_frame, font=('Helvetica', 11), selectbackground='Gold')
+
+scroll_bar = Scrollbar(listbox_frame, orient=VERTICAL)
+scroll_bar.pack(side=RIGHT, fill=BOTH)
+
+playlist.config(yscrollcommand=scroll_bar.set)
+
+scroll_bar.config(command=playlist.yview)
+
+playlist.pack(fill=BOTH, padx=5, pady=5)
+
+Label(song_frame, text='CURRENTLY PLAYING:', bg='LightBlue', font=('Times', 10, 'bold')).place(x=5, y=20)
+
+song_lbl = Label(song_frame, textvariable=current_song, bg='Goldenrod', font=("Times", 12), width=25)
+song_lbl.place(x=150, y=20)
+
+pause_btn = Button(button_frame, text='Pause', bg='LightBlue', font=("Georgia", 13), width=7,
+                    command=lambda: pause_song(song_status))
+pause_btn.place(x=15, y=10)
+
+stop_btn = Button(button_frame, text='Stop', bg='LightBlue', font=("Georgia", 13), width=7,
+                  command=lambda: stop_song(song_status))
+stop_btn.place(x=105, y=10)
+
+play_btn = Button(button_frame, text='Play', bg='LightBlue', font=("Arial", 13), width=7,
+                  command=lambda: play_song(current_song, playlist, song_status))
+play_btn.place(x=195, y=10)
+
+resume_btn = Button(button_frame, text='Resume', bg='LightBlue', font=("Arial", 13), width=7,
+                    command=lambda: resume_song(song_status))
+resume_btn.place(x=285, y=10)
+
+load_btn = Button(button_frame, text='Load Directory', bg='LightBlue', font=("Arial", 13), width=35,
+                  command=lambda: load(playlist))
+load_btn.place(x=10, y=55)
+
+Label(root, textvariable=song_status, bg='SteelBlue', font=('Times', 9), justify=LEFT).pack(side=BOTTOM, fill=X)
+
+root.update()
 root.mainloop()
